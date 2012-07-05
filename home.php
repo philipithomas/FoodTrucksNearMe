209a26a81ef2623b4c8a9d103ec9ad1dd6c8727e
Philip I. Thomas <mail@philipithomas.com>
2012-07-05 00:45:02 -0500

    Routing complete

---
+++ home.php
@@ -0,0 +1,10 @@
+<?php
+// redirected here from /home via .htaccess
+
+// This file will use the landing page on github
+
+echo "Welcome home";
+
+
+
+?>
--- router.php
+++ router.php
@@ -1,5 +1,17 @@
 <?php
-
-echo "I'm routing you now";
-
+/* This is the landing page for the website. If users have already gone to a city, they are directed to that city. Otherwise, they are directed to the home page where they can select a new city.
+*/
+if (isset($_SESSION['city'])) {
+	
+	// If they already have a city. 
+	// Mod rewrite redirects to city.php
+	header("Location: /city/".strtolower($_SESSION['city']));
+	exit;
+}
+else {
+	// If no city, go to home page to select a city
+	header("Location: /home"));
+	exit;
+	
+}
 ?>
