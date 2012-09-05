c617caa0e2520701efa12fe4a43b1da9394dc41d
Philip I. Thomas <mail@philipithomas.com>
2012-09-04 23:52:25 -0500

    Secure elements

--- premium.php
+++ premium.php
@@ -43,25 +43,25 @@ echo '<!DOCTYPE html>
     <meta name="description" content="The Dashboard of Local Food Trucks">
 	<meta name="viewport" content="user-scalable=no, width=device-width">
 
-    <!-- Le HTML5 shim, for IE6-8 support of HTML ehref="mailto:support@foodtrucksnearme.com"lements -->
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
     <!--[if lt IE 9]>
-      <script src="http://html5shim.google.com/svn/trunk/html5.js"></script>
+      <script src="https://html5shim.google.com/svn/trunk/html5.js"></script>
     <![endif]-->
 
-	<script language="javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
+	<script language="javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
 	
     <!-- Le styles -->
-    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="https://www.foodtrucksnearme.com/css/bootstrap.css" rel="stylesheet">
     <style type="text/css">
       body {
         padding-top: 60px;
         padding-bottom: 40px;
       }
     </style>
-    <link href="../css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="https://www.foodtrucksnearme.com//css/bootstrap-responsive.css" rel="stylesheet">
 
     <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="/img/favicon.ico">
+    <link rel="shortcut icon" href="https://www.foodtrucksnearme.com/img/favicon.ico">
 
 
   </head>
@@ -75,7 +75,7 @@ echo '<!DOCTYPE html>
                         <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </a>
-          <a class="brand" href="#"><img src="/img/header/HOME.png"> Food Trucks Near Me - Premium</a>
+          <a class="brand" href="#"><img src="https://www.foodtrucksnearme.com/img/header/HOME.png"> Food Trucks Near Me - Premium</a>
           <div class="nav-collapse">
             <ul class="nav">
               <li><a href="/home">Home</a></li>
@@ -100,7 +100,9 @@ echo '<!DOCTYPE html>
 	
 	function bottom () {
 	echo '  <footer><hr />
-      <center>  <p><?php footerText();?></p></center>
+      <center>  <p>';
+      echo footerText();
+      echo '</p></center>
       </footer>
 
     </div> <!-- /container -->
@@ -108,13 +110,13 @@ echo '<!DOCTYPE html>
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../js/jquery.js"></script>
-    <script src="../js/bootstrap-alert.js"></script>
-    <script src="../js/bootstrap-modal.js"></script>
-    <script src="../js/bootstrap-dropdown.js"></script>
-    <script src="../js/bootstrap-scrollspy.js"></script>
-    <script src="../js/bootstrap-button.js"></script>
-    <script src="../js/bootstrap-collapse.js"></script>
+    <script src="https://www.foodtrucksnearme.com/js/jquery.js"></script>
+    <script src="https://www.foodtrucksnearme.com/js/bootstrap-alert.js"></script>
+    <script src="https://www.foodtrucksnearme.com/js/bootstrap-modal.js"></script>
+    <script src="https://www.foodtrucksnearme.com/js/bootstrap-dropdown.js"></script>
+    <script src="https://www.foodtrucksnearme.com/js/bootstrap-scrollspy.js"></script>
+    <script src="https://www.foodtrucksnearme.com/js/bootstrap-button.js"></script>
+    <script src="https://www.foodtrucksnearme.com/js/bootstrap-collapse.js"></script>
 	
   </body>
 </html>
@@ -126,4 +128,4 @@ echo '<!DOCTYPE html>
 
 
 
-?>gg
+?>
