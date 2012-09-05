b29514944c0c8be35e7f3b26f7635ca28d804741
Philip I. Thomas <mail@philipithomas.com>
2012-09-04 23:40:04 -0500

    force ssl

--- premium.php
+++ premium.php
@@ -6,7 +6,7 @@ require_once('./inc/functions.inc');
 // Force SSL
 if($_SERVER["HTTPS"] != "on") {
    header("HTTP/1.1 301 Moved Permanently");
-   header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
+   header("Location: https://www.foodtrucksnearme.com/premium"]);
    exit();
 }
 
